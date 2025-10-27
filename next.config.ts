5a6374db38dbdb62b453eee4b6412e2e65981518
Huy Quang Nguyen <quanghuy1242@gmail.com>
2025-10-27 12:24:46 +0700

    Add crossOrigin configuration to Next.js settings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  crossOrigin: 'anonymous',
 };
 
 export default nextConfig;
